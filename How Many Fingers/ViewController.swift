9e2796ee8ae015cf11de746a9e23a25fa543e70d
Mat Sletten <matsletten@reception-desk.bbbb.net>
2015-01-30 11:32:01 -0800

    Updates

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var guessText: UITextField!
+    
+    @IBAction func guessButtonPressed(sender: AnyObject)
+    {
+        var numberOfFingers = Int(arc4random() % 6)
+       //can convert numberOfFingers to string by creating a variable and calling method String(numberOfFingers). Also, I made sure that a user has entered a guess before either if or else evaluates.
+        if (numberOfFingers == guessText.text.toInt() && guessText.text.toInt() != nil)
+       {
+        guessMessageText.text = "Congratulations"
+        }
+        else if (guessText.text.toInt() == nil)
+        {
+            guessMessageText.text = "Please enter a guess"
+        }
+        else
+       {
+        guessMessageText.text = "Please Guess Again"
+        }
+    }
+    
+    @IBOutlet weak var guessMessageText: UILabel!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
